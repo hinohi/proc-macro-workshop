993e051ee4bf2eb00bf6c921c750b5eeec882e5d
Nakayama Daiju <42.daiju@gmail.com>
2020-01-26 17:06:16 +0900

    [builder] 02 impl empty builder method

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -2,10 +2,17 @@ extern crate proc_macro;
 
 use proc_macro::TokenStream;
 use syn::{parse_macro_input, DeriveInput};
+use quote::quote;
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = parse_macro_input!(input as DeriveInput);
+    let input = parse_macro_input!(input as DeriveInput);
+    let name = input.ident;
 
-    TokenStream::new()
+    let expanded = quote!{
+        impl #name {
+            pub fn builder() {}
+        }
+    };
+    TokenStream::from(expanded)
 }
